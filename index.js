7fdf05b34187a6adf29753abf5f0511fbb0f8746
Pau-Isach-Noguera <pau.nughy@gmail.com>
2022-06-04 19:15:11 +0200

    Proyecto Upgrade-Avengers

---
+++ index.js
@@ -0,0 +1,12 @@
+//PROYECTO FINAL DE NODE -> Upgrade-Avengers
+
+//Primero tenemos que instalar librerias mediante la terminal. 
+//Estas instalaciones nos genera el archivo package.json.
+// npm i body-parser
+// npm i cors
+// npm i dotenv
+// npm i express
+// npm i mongoose
+
+//También instalaremos la dependencia de desarrollo siguente.
+//npm install --save-dev nodemon
